fed32ef79e6edc31b305de2be9894a1fea64f2ba
chandan7193 <chandan.singh7193@gmail.com>
2017-08-21 22:38:43 +0530

    Image and Title visible on Ui

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,7 +5,7 @@ const VideoList =(props) => {
 	const videoItems = props.videos.map((video) =>{
 		return <VideoListItem key={video.etag} video = {video} />
 		});
-	
+
 	return (
 		<ul className="col-md-4 list-group">
 			{ videoItems }
