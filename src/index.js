41affc99e380e1727ab643a325f482c29d218da8
chandan7193 <chandan.singh7193@gmail.com>
2017-08-22 23:27:31 +0530

    Throttling the Youtube search usinf lodash

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
@@ -16,20 +17,25 @@ class App extends Component{
 		videos: [],
 		selectedVideo: null
 	};
+	this.videoSearch('')
 
 
-	YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-		this.setState({
-			videos: videos,
-			selectedVideo: videos[0]
-	 		});
-		});
+	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+		 		});
+			});
 	}
 
 render(){
+	const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300)
 	return (
 		<div>
-			<SearchBar/>
+			<SearchBar onSearchTermChange={videoSearch}/>
 			<VideoDetail video={this.state.selectedVideo} />
 			<VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
 
